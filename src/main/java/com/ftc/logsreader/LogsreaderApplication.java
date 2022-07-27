77dcdaa7694ff4ec3e2217df8faca9faa8b5a0cf
Anton_Ramanau <anton61499@gmail.com>
2022-07-27 14:01:15 +0300

    init project

---
+++ src/main/java/com/ftc/logsreader/LogsreaderApplication.java
@@ -0,0 +1,13 @@
+package com.ftc.logsreader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogsreaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogsreaderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ftc/logsreader/LogsreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftc.logsreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogsreaderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
