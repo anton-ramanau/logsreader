ab8c37cd5931cfb639349af05c72fc741bba8704
Anton_Ramanau <anton61499@gmail.com>
2022-07-27 18:31:44 +0300

    Refactoring LogReaderServiceImpl and project version.

--- src/main/java/com/ftc/logsreader/service/LogReaderServiceImpl.java
+++ src/main/java/com/ftc/logsreader/service/LogReaderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ftc.logsreader.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -12,12 +13,16 @@ import java.util.List;
 @Service
 public class LogReaderServiceImpl implements LogReaderService {
 
-    @Value("${PATH_TO_LOG_FILE}")
-    private String pathToLogFile;
+    public static String ENV_NAME_PATH_TO_LOG_FILE = "PATH_TO_LOG_FILE";
+    private final Environment environment;
+
+    public LogReaderServiceImpl(Environment environment) {
+        this.environment = environment;
+    }
 
     @Override
     public List<String> getLogsAsLines() {
-        Path logPath = Paths.get(pathToLogFile);
+        Path logPath = Paths.get(environment.getProperty(ENV_NAME_PATH_TO_LOG_FILE));
         try {
             return Files.readAllLines(logPath);
         } catch (IOException e) {
